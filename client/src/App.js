11818c654dcc5956f29e7eb6572b9f31126cb078
dawsonsewell <dsewell@uwo.ca>
2021-02-09 09:13:11 -0500

    Created react project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+    return (
+      <div id="app">
+       <div>
+         Header
+        </div>
+        <div>
+          Main part
+        </div>
+      </div>
+    );
+  }
+
+export default App;
